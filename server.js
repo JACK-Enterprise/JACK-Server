37a4e6c8ea1617b34f75208f7755dd0092f979a0
Amram ELBAZ <elbaz.amram@gmail.com>
2016-03-15 11:05:27 +0100

    Port number correction

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello Jack World!');
 });
 
-app.listen(3000, function () {
+app.listen(4242, function () {
   console.log('Example app listening on port 4242!');
 });
