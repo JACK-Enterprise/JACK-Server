d054ed8ccfa34705f454d4d337f187dda23bed01
Amram Elbaz <elbaz.amram@gmail.com>
2016-04-26 11:34:04 +0200

    Authentication and database system added.

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root'
+    },
+	'database': 'jackserver',
+    'users_table': 'users'
+};
