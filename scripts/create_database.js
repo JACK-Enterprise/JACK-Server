a882d71eb1e9c1120e118e4db92aaf3ceb0ccc94
Marma <elbaz.amram@gmail.com>
2016-06-19 21:56:31 +0200

    Database script modified to change the database model. User now will be created with an email and an admin rights indicator

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -14,9 +14,12 @@ CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
     `username` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
+    `email` CHAR(100) NOT NULL, \
+    `admin` TINYINT(1) NOT NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+    UNIQUE INDEX `username_UNIQUE` (`username` ASC), \
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) \
 )');
 
 console.log('Success: Database Created!')
